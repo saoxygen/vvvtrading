687ae77cf287b7f083c52df4c08e80d18f3b0e94
saoxygen <104069898+saoxygen@users.noreply.github.com>
2024-03-19 02:00:47 +0200

    first commit of completed website

---
+++ index.js
@@ -0,0 +1,11 @@
+// var bars = document.getElementById("bars");
+// var menu = document.getElementById("menu2");
+
+// bars.addEventListener("click", function () {
+//     if (menu.className === "motion") {
+//         menu.classList.remove("motion")
+//     } else {
+//         menu.classList.add("motion")
+//     }
+// })
+
